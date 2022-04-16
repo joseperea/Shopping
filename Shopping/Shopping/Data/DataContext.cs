051aeade993ef7e87ca8ded50172e9649508dccc
Jose Hernando Valencia Perea <johevape03@hotmail.com>
2022-04-16 09:18:19 -0500

    Se crea proyecto Shopping

---
+++ Shopping/Shopping/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+
+namespace Shopping.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres", MinimumLength = 4)]
+        public string Name { get; set; }
+    }
+}
